01d2d89bc5c298dbe3f3051dae09bda7918892f3
arrahmanulhakim <hakimarrahmannul@gmail.com>
2023-03-12 17:44:13 +0700

    clear2

--- soal2.dart
+++ soal2.dart
@@ -1,3 +1,34 @@
-void main() {
-  print("subjek");
+class Calculator {
+  double operan1;
+  double operan2;
+
+  Calculator(this.operan1, this.operan2) {}
+
+  void isiOperan1(double x) {
+    operan1 = x;
+  }
+
+  void isiOperan2(double x) {
+    operan2 = x;
+  }
+
+  double tambah() {
+    return operan1 + operan1;
+  }
+
+  double kurang() {
+    return operan1 - operan2;
+  }
+
+  double kali() {
+    return operan1 * operan2;
+  }
+
+  double bagi(double numerator, double denominator) {
+    return numerator / denominator;
+  }
+
+  double pangkat(double operan) {
+    return operan * operan;
+  }
 }
